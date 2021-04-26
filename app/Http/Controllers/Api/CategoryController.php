6d6ca4b8895873e7ef4003f66c184c99282b36ad
vagrant <vagrant@homestead>
2021-04-26 01:38:54 +0000

    model has relations which is null not got

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -11,7 +11,7 @@ class CategoryController extends Controller
     public function index()
     {
 
-        return Category::all();
+        return Category::has('products')->get();
     }
 
     public function show(Category $category)
