2d39238b26312079a87c177bf57af8da04bf93b3
vagrant <vagrant@homestead>
2025-02-22 08:04:32 +0000

    git update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Pricing and buying system from comlibra electronic</title>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        
         <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
         <style>
             body {
